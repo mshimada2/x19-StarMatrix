ed34d1fc176dd5a896c95723fe32e381d263e0f8
mshimada <mshimada2@mail.csuchico.edu>
2017-10-20 15:30:06 -0700

    Resize

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(){
 
-vector <vector <int> > star = '*';
+vector <vector <int> > star;
 
 int row;
 
@@ -19,17 +19,26 @@ cin>>row;
 cout<<endl<<"Next, please enter a number value for the column (e.g. 4): ";
 cin>>column;
 
-
+    star.resize(row);
 
 
 for(int row = 0; row < star.size(); row++){
 
+    star[row].resize(column);
+    
+    
     for(int column = 0; column < star[row].size(); column++){
 
+    
+    star[row][column]
+
+    
     cout<<endl<<star[row][column];
 
+    
     }
 
+    
     }
 
 return 0;
